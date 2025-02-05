4164756ba95167ce6ebbf97de958d24b8c051f37
unknown <psh4060144@gmail.com>
2025-02-05 22:46:55 +0900

    20250205 2245 homework

---
+++ AlgorithmClass/right_of_a_view.py
@@ -0,0 +1,34 @@
+# View_조망권
+
+T = 10  # test case의 갯수가 10개.
+
+for i in range(1, T+1):
+    
+    N = int(input())  # 건물의 갯수를 받아옴
+    arr = list(map(int, input().split()))  # 건물의 높이를 받아옴
+    left_highest = 0  # 좌측, 우측을 나누어 생각하기 위해 좌측의 높은 건물을 인수로 지정.
+    right_highest = 0  # 우측의 높은 건물을 인수로 지정.
+    highest = 0  # left_highest, right_highest를 비교하여 나 이외에 가장 높은 건물을 찾음.
+    right_of_a_view = 0  # 조망권 총합을 더하기 위해 인수를 설정.
+    
+    for j in range(2, N-2):  # 가장 왼쪽, 가장 오른쪽 두 칸은 반드시 0이기 때문에 그 사이 값만 비교하면 됨.
+        
+        if arr[j-2] > arr[j-1]:  # 왼쪽의 가장 높은 건물을 찾는 부분.
+            left_highest = arr[j-2]
+        else:
+            left_highest = arr[j-1]
+        
+        if arr[j+1] < arr[j+2]:  # 오른쪽의 가장 높은 건물을 찾는 부분.
+            right_highest = arr[j+2]
+        else:
+            right_highest = arr[j+1]
+        
+        if left_highest > right_highest:  # left_highest와 right_highest를 비교하여 가장 높은 건물을 찾는 부분.
+            highest = left_highest
+        else:
+            highest = right_highest
+        
+        if arr[j] > highest:  # 해당 건물과 비교하여 조망권을 더함.
+            right_of_a_view += arr[j] - highest
+            
+    print(f'#{i} {right_of_a_view}')
